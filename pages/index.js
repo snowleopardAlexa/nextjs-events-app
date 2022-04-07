ca52957c25488ccc4c5bf924053968a2d2eedec4
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-06 21:23:50 -0400

    add dummy data with functions

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import { getFeaturedEvents } from "../data/dummy-data"
+
+
 const HomePage = () => {
+
+// featured events function
+const featuredEvents = getFeaturedEvents()
+
   return (
-    <div>homepage</div>
+    <div>
+      
+    </div>
   )
 }
 
