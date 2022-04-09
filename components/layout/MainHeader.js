77f18592498b2ceaeb70396de7704c4171d6686e
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-09 19:54:11 -0400

    add main header

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import { collectAssets } from "next/dist/build/webpack/plugins/middleware-plugin"
+import Link from "next/link"
+import styles from './main-header.module.css'
 
 const MainHeader = () => {
   return (
-    <div>MainHeader</div>
+    <header className={navigation.header}>
+        <div className={navigation.logo}>
+          <Link href="/">NextEvents</Link>
+        </div>
+        <nav className={styles.navigation}>
+          <ul>
+              <li>
+                  <Link href="/events">Browse All Events</Link>
+              </li>
+          </ul>
+        </nav>
+    </header>
   )
 }
 
