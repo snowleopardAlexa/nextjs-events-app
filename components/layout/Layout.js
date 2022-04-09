a16288b0d80efc99c59f6a6a2fb3ad10c92eadab
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-09 19:56:28 -0400

    add alayout to app.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,16 @@
 import { Fragment } from 'react'
+import MainHeader from './MainHeader'
 
 const Layout = (props) => {
   return (
+    <MainHeader>
     <Fragment>
         <header></header>
         <main>
             {props.children}
         </main>
     </Fragment>
+    </MainHeader>
   )
 }
 
