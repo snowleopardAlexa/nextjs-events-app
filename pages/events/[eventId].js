8090b03cbe30916f74d95c6212f26a66f79bea01
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-09 15:25:25 -0400

    add getEventById to eventId

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,18 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../dummy-data'
+
 const EventDetailPage = () => {
+
+  const router = useRouter()
+
+  const eventId = router.query.eventId
+  const event = getEventById(eventId)
+
+  // if event is falsey 
+  if (!event) {
+    return <p>No event found</p>
+  }
+
   return (
    <div>
      event detail page
