a7964c06ea5dc5008a66b83ee00af2feb8973246
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-09 21:02:29 -0400

    add events search modules css

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
+import { Fragment } from 'react'
 import { getAllEvents } from '../../data/dummy-data'
 import EventList from '../../components/events/EventList'
+import EventsSearch from "../../components/events/EventsSearch"
 
 const AllEventsPage = () => {
   const events = getAllEvents()
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />  
-    </div>
+    </Fragment>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import { getFeaturedEvents } from "../data/dummy-data"
-import EventList from "../components/events/EventList"
+import { getFeaturedEvents } from "../data/dummy-data";
+import EventList from "../components/events/EventList";
 
 const HomePage = () => {
-  
-const featuredEvents = getFeaturedEvents()
+  const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
