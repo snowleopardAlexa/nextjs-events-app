de977a703a28f81a44d2b0c65246dde3747c6d74
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-09 20:06:39 -0400

    add all events page

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -1,12 +1,11 @@
-import { collectAssets } from "next/dist/build/webpack/plugins/middleware-plugin"
 import Link from "next/link"
 import styles from './main-header.module.css'
 
 const MainHeader = () => {
   return (
-    <header className={navigation.header}>
-        <div className={navigation.logo}>
-          <Link href="/">NextEvents</Link>
+    <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/">Pets Events</Link>
         </div>
         <nav className={styles.navigation}>
           <ul>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,13 @@
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+
 const AllEventsPage = () => {
+  const events = getAllEvents()
+
   return (
-    <div>events page</div>
+    <div>
+      <EventList items={events} />  
+    </div>
   )
 }
 
